14667ec0563780efed62c1c72e38243c846e5c4f
Marius Reusch <marius.reusch@gmail.com>
2019-02-06 13:16:58 +0100

    clean up master

--- src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
+++ src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
@@ -6,9 +6,6 @@ public class Application {
 
   public static void main(String[] args) {
 
-    CustomerController customerController = new CustomerController();
-
-    customerController.deactivateCustomerAccount("1");
   }
 
 }
