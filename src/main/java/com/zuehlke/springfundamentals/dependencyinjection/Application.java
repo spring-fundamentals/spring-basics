62ad77a5210e99c5c65e8efad04e81b95827a5ca
Marius Reusch <marius.reusch@gmail.com>
2019-02-06 15:58:49 +0100

    clean up

--- src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
+++ src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
@@ -1,15 +1,16 @@
 package com.zuehlke.springfundamentals.dependencyinjection;
 
-import com.zuehlke.springfundamentals.dependencyinjection.configuration.AppConfig;
 import com.zuehlke.springfundamentals.dependencyinjection.controller.CustomerController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
 
-    ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    ApplicationContext ctx = new AnnotationConfigApplicationContext(Application.class);
 
     CustomerController customerController = ctx.getBean(CustomerController.class);
 
