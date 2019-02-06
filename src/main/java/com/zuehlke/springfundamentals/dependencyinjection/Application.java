b1001a6bee5a6ccb63b3713b3494b3c985666500
Marius Reusch <marius.reusch@gmail.com>
2019-02-06 15:46:40 +0100

    clean up

--- src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
+++ src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
@@ -1,18 +1,20 @@
 package com.zuehlke.springfundamentals.dependencyinjection;
 
-import com.zuehlke.springfundamentals.dependencyinjection.configuration.AppConfig;
 import com.zuehlke.springfundamentals.dependencyinjection.controller.CustomerController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.zuehlke.springfundamentals.dependencyinjection")
 public class Application {
 
   public static void main(String[] args) {
 
-    ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 
-    CustomerController customerController = ctx.getBean(CustomerController.class);
+    CustomerController customerController = context.getBean(CustomerController.class);
 
     customerController.deactivateCustomerAccount("1");
+
   }
 }
