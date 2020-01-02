3b72ad94d7be2c193658e29dcb847e42a7201f05
Joachim <joachim.prinzbach@gmail.com>
2020-01-02 13:04:50 +0100

    Fix compile error

--- src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
+++ src/main/java/com/zuehlke/springfundamentals/dependencyinjection/Application.java
@@ -1,7 +1,5 @@
 package com.zuehlke.springfundamentals.dependencyinjection;
 
-import com.zuehlke.springfundamentals.dependencyinjection.controller.CustomerController;
-
 public class Application {
 
   public static void main(String[] args) {
