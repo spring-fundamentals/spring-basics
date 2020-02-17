ec6cf609ac79f2e282d210569d11d286e794af31
Zillessen, Sebastian <sebastian.zillessen@zuehlke.com>
2020-02-17 14:06:53 +0100

    internal: Fix diamond operator InMemoryDatabase.java

--- src/main/java/com/zuehlke/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
+++ src/main/java/com/zuehlke/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class InMemoryDatabase {
 
-  public static final Map<String, Customer> DATABASE = new HashMap<>() {{
+  public static final Map<String, Customer> DATABASE = new HashMap<String, Customer>() {{
     put("1", new Customer("1", "Hans Wurst", new MailingAddress("Hauptstrasse 2", "Zürich"), "hans@wurst.ch"));
     put("2", new Customer("2", "Peter Peterson", new MailingAddress("Bahnhofsweg 100", "Bern"), "peter@peterson.ch"));
   }};
