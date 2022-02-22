46493ce6a7fb9ba3192e98fa24c904d8c3617bfe
Marius Reusch <marius.reusch1@swisscom.com>
2022-02-22 20:22:47 +0100

    make fields final

--- src/main/java/com/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
+++ src/main/java/com/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
@@ -8,8 +8,8 @@ import java.util.Map;
 
 public class InMemoryDatabase {
 
-  public static final Map<String, Customer> DATABASE = new HashMap<String, Customer>() {{
-    put("1", new Customer("1", "Hans Wurst", new MailingAddress("Hauptstrasse 2", "Zürich"), "hans@wurst.ch"));
-    put("2", new Customer("2", "Peter Peterson", new MailingAddress("Bahnhofsweg 100", "Bern"), "peter@peterson.ch"));
+  public static final Map<String, Customer> DATABASE = new HashMap<>() {{
+    put("1", new Customer("1", "Gerd Müller", new MailingAddress("Hauptstrasse 2", "Zürich"), "gerd@mueller.ch"));
+    put("2", new Customer("2", "Sepp ˚Maier", new MailingAddress("Bahnhofsweg 100", "Bern"), "sepp@maier.ch"));
   }};
 }
