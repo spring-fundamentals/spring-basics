c7aad4162beffd09fbdde9c183bbb3f8e1682afe
Marius Reusch <marius.reusch1@swisscom.com>
2023-10-22 19:54:50 +0200

    change test data

--- src/main/java/com/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
+++ src/main/java/com/springfundamentals/dependencyinjection/dataaccess/InMemoryDatabase.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class InMemoryDatabase {
 
     public static final Map<String, Customer> DATABASE = new HashMap<>() {{
-        put("1", new Customer("1", "Gerd Müller", new MailingAddress("Hauptstrasse 2", "Zürich"), "gerd@mueller.ch"));
-        put("2", new Customer("2", "Sepp Maier", new MailingAddress("Bahnhofsweg 100", "Bern"), "sepp@maier.ch"));
+        put("1", new Customer("1", "Albus Dumbledore", new MailingAddress("High Street 1", "Hogwarts"), "albus@hogwarts.edu"));
+        put("2", new Customer("2", "Rubeus Hagrid", new MailingAddress("Hagrid's House", "Hogwarts"), "rubeus@hogwarts.edu"));
     }};
 }
